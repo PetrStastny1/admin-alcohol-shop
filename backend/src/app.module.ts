c3ebb8650f61bb82347dee8198b3ea9046daa611
PetrStastny1 <Petr_Stastny@seznam.cz>
2025-11-04 10:35:46 +0100

    fix cesty k angular buildu

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -22,7 +22,7 @@ import { OrdersModule } from './orders/orders.module';
   imports: [
     // --- Servírování Angular buildu (frontend/dist/frontend/browser) ---
     ServeStaticModule.forRoot({
-      rootPath: join(process.cwd(), 'backend', 'frontend', 'dist', 'frontend', 'browser'),
+      rootPath: join(process.cwd(), 'frontend', 'dist', 'frontend', 'browser'),
       exclude: ['/graphql*', '/api*'],
     }),
 
