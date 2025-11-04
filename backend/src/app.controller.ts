e5a4be8214cd045776b916e4f7094e0adc45a45f
PetrStastny1 <Petr_Stastny@seznam.cz>
2025-11-04 11:13:21 +0100

    fix servírování angular index

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  serveFrontend(@Res() res: Response) {
+    const indexPath = join(__dirname, '..', 'frontend', 'dist', 'frontend', 'browser', 'index.html');
+    res.sendFile(indexPath);
   }
 }
